24ca0402531b41349ce12658012d19caca3c3eb9
Enrique Montemayor <eemontemayor@live.com>
2019-02-28 11:06:32 -0600

    finish first task -namespacing

--- scripts/Items.js
+++ scripts/Items.js
@@ -1,6 +1,22 @@
-const Item = (function(){
-    const foo = "bar";
-    return {
+'use strict';
+/* global cuid */
 
+const Item = (function(){
+
+function validateName(name){
+    if(!name){
+        throw TypeError ("Name does not exist!");
     }
-}() );
+}
+
+function create(name){
+    return {
+        id: cuid(),
+    name: name,
+    checked: false,
+    };
+}    
+    return {
+        validateName, create
+    };
+}());
