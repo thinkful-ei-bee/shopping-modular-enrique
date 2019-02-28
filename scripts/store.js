a1827e3815b729f6474216747df4dfde6f4a64ac
Enrique Montemayor <eemontemayor@live.com>
2019-02-28 13:48:03 -0600

    finished third task-namespacing

--- scripts/store.js
+++ scripts/store.js
@@ -11,10 +11,46 @@ const store = (function (){
     const hideCheckedItems= false;
     const searchTerm= '';
     
+      function findById(id){
+        items.find(id => id === items.id);
+      }
+
+      function addItem(name){
+        try{
+          Item.validateName(name);
+          this.items.push(Item.create(name));
+        } catch (error){
+          console.log('Cannot add item: ' + error.message);
+        }
+      }
+
+      function findAndToggleChecked(id){
+        this.findById(id) === !this.findById(id);
+      }
+
+      function findAndUpdateName(id, newName){
+        try{
+          Item.validateName(newName);
+          this.findById(id).name = newName;
+        } catch (error){
+          console.log(`Cannot update name: ${error.message}`);
+        }
+      }
+      
+      function findAndDelete(id){
+       this.items = this.items.filter(i => i.id !== id);
+      }
+
     return {
        items,
        hideCheckedItems,
        searchTerm, 
+       findById,
+       addItem,
+       findAndToggleChecked,
+       findAndUpdateName,
+       findAndDelete,
+
     }
 
 }() );
